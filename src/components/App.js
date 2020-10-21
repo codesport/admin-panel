06328ed5ad7bd369008a342bd6dc1540e52b1179
Code Sport I/O <CodeSportCares@codesport.io>
2020-10-21 10:02:16 -0400

    beautifying code for publication to github

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Controller from './Controller';
 
-
 function App(){
-
     return(
         <React.Fragment>
             <Controller />
         </React.Fragment>
-
     )
-
-
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,12 @@ function Header(props){
     }
 
     /**
-     * "Figure out the absolute minimal representation of the state your application needs
-     * and compute everything else you need on-demand."
+     * Pro Tip 1: "Figure out the absolute minimal representation of the state your 
+     * application needs and compute everything else you need on-demand."
      * 
-     * "If you’re building a list, store the list in an array. And, don’t keep a separate 
-     * state variable for the count. Instead, when you want to render the list count, take
-     * the length of the array"
+     * Pro Tip 2: If you’re building a list, store the list in an array. And, don’t
+     * keep a separate state variable for the count. Instead, when you want to render
+     * the list count, take the length of the array.
      * 
      * @tutorial https://reactjs.org/docs/thinking-in-react.html
      */
@@ -24,7 +24,6 @@ function Header(props){
             <h3 style={styledHeader}>Arroyo Family Farms Inventory List</h3>
             <h4 style={styledHeader}>Total Unique Items: {props.masterList.length}</h4>
         </React.Fragment>
-
     )
 }
 
